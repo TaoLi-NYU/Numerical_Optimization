d1f668760f0ef8cdb9c360976d252261278322d3
Tao Li <taoli@10-18-58-235.dynapool.wireless.nyu.edu>
2019-04-13 15:48:32 -0400

    modified back_upper.m

--- back_upper.m
+++ back_upper.m
@@ -1,18 +1,19 @@
 function [y] = back_upper(U,x)
-%The is a funciton solves a given upper triangular system Ux=0 using backward
+%This funciton solves a given upper triangular system Ux=0 using backward
 %substition
 %  input: U the upper traingular for its diagonal elements, only u_{n,n}=0;
 %         x the last component of the solution 
 %  Ouput: y the solution given by U and x
-N=length(U);
-y=zeros(N,1);
+N=length(U); % the dimension of the matrix
+y=zeros(N,1); %preallocation for the solution
 y(N)=x;
-for i=(N-1):-1:1
+for i=(N-1):-1:1 %from the bottom to the top
     numerator=0;
     for j=(i+1):N
-        numerator=numerator+U(i,j)*y(j);
+        numerator=numerator+U(i,j)*y(j); %compute the numerator
     end
-    y(i)=-numerator/U(i,i);
+    fprintf('%8.6e\n',numerator);
+    y(i)=-numerator/U(i,i); %i-th component of the solution 
 end
 
 end
